4c8ef8e28a09007bda41bdfbf31e636d23393b54
Luis Jara <23040089@alumno.utc.edu.mx>
2026-06-12 19:22:03 -0600

    se realizan direcciones de navegacion y se retira pagina standar de laravel 13

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/a', function () {
     return view('home.index');
 });
 
